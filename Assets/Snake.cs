00935bdf00753ee4dc1ece864fab6d7950431a3e
Lucas Campos <lcmc@cin.ufpe.br>
2023-10-19 06:13:58 -0300

    snake behaviour created

--- Assets/Snake.cs
+++ Assets/Snake.cs
@@ -5,32 +5,95 @@ using UnityEngine;
 public class Snake : MonoBehaviour
 {
     private Vector2 _direction = Vector2.right;
+    private List<Transform> _segments = new List<Transform>();
+
+    public Transform segmentPrefab;
+
+    public int initialSize = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ResetState();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)){
+        if (Input.GetKeyDown(KeyCode.W))
+        {
             _direction = Vector2.up;
-        } else if (Input.GetKeyDown(KeyCode.S)){
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
             _direction = Vector2.down;
-        } else if (Input.GetKeyDown(KeyCode.A)){
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
             _direction = Vector2.left;
-        } else if (Input.GetKeyDown(KeyCode.D)){
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
             _direction = Vector2.right;
         }
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
+        for (int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + _direction.x,
             Mathf.Round(this.transform.position.y) + _direction.y,
             0f
         );
+    }
+
+    void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+    void ResetState()
+    {
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        for (int i = 1; i < initialSize; i++)
+        {
+            Grow();
+        }
+
+        this.transform.position = Vector3.zero;
+    }
+
+    void Shrink()
+    {
+        Transform segment = _segments[_segments.Count - 1].transform;
+        _segments.Remove(segment);
+        Destroy(segment.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Food"))
+        {
+            Grow();
+        }
+        else if (other.CompareTag("Wall"))
+        {
+            Debug.Log("Game Over");
+        }
     }
 }
