f1f7fd29d814896971bc0aeca22961a9aaef9edf
Lucas Campos <lcmc@cin.ufpe.br>
2023-10-19 08:32:03 -0300

    created gamemanager, add gameover screen, and restart game

---
+++ Assets/GameManager.cs
@@ -0,0 +1,94 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; // Instância única do GameManager
+    private int score; // Score atual
+    private int highScore; // Highscore
+
+    public TMP_Text scoreText; // Texto para exibir o score
+    public TMP_Text highScoreText; // Texto para exibir o highscore
+    public TMP_Text endGameText; // Texto para exibir o highscore
+
+    public bool isGameOver = true; // Flag para indicar se o jogo acabou
+
+    public delegate void GameStartDelegate();
+    public static event GameStartDelegate OnGameStart;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        // Carregar o highscore salvo (caso exista)
+        highScore = PlayerPrefs.GetInt("HIGH", 0);
+        RefreshText();
+    }
+
+    private void Update()
+    {
+        if (!isGameOver)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartGame();
+        }
+    }
+
+
+    private void StartGame()
+    {
+        isGameOver = false;
+        endGameText.gameObject.SetActive(false);
+        ResetScore();
+
+        OnGameStart?.Invoke();
+    }
+
+    public void EndGame()
+    {
+        isGameOver = true;
+        endGameText.gameObject.SetActive(true);
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        RefreshText();
+    }
+
+
+
+    public void AddScore(int points)
+    {
+        score += points;
+
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("HIGH", highScore);
+        }
+
+        RefreshText();
+    }
+
+    public void RefreshText()
+    {
+        scoreText.text = "SCORE: " + score.ToString();
+        highScoreText.text = "HIGH: " + highScore.ToString();
+    }
+}
--- Assets/Snake.cs
+++ Assets/Snake.cs
@@ -8,20 +8,61 @@ public class Snake : MonoBehaviour
     private Vector2 _nextDirection;
     private List<Transform> _segments = new List<Transform>();
 
-    public Transform segmentPrefab;
+    [SerializeField] private Transform segmentPrefab;
 
-    public int initialSize = 2;
+    [SerializeField] private int initialSize = 2;
 
+    private float moveTimer;
+    [SerializeField] private float moveTimerMax = 0.1f;
 
+    private GameManager _gameManager;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
+    {
+        _gameManager = GameManager.instance;
+        GameManager.OnGameStart += ResetState;
+    }
+
+    private void Start()
     {
         ResetState();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        if (_gameManager.isGameOver)
+        {
+            return;
+        }
+
+        HandleInput();
+        HandleMoviment();
+    }
+
+    private void HandleMoviment()
+    {
+        moveTimer += Time.deltaTime;
+
+        if (moveTimer < moveTimerMax)
+        {
+            return;
+        }
+
+        moveTimer -= moveTimerMax;
+
+        for (int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + _direction.x,
+            Mathf.Round(this.transform.position.y) + _direction.y,
+            0f
+        );
+    }
+
+    private void HandleInput()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
 
@@ -44,21 +85,7 @@ public class Snake : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
-    {
-        for (int i = _segments.Count - 1; i > 0; i--)
-        {
-            _segments[i].position = _segments[i - 1].position;
-        }
-
-        this.transform.position = new Vector3(
-            Mathf.Round(this.transform.position.x) + _direction.x,
-            Mathf.Round(this.transform.position.y) + _direction.y,
-            0f
-        );
-    }
-
-    void Grow()
+    private void Grow()
     {
         Transform segment = Instantiate(this.segmentPrefab);
         segment.position = _segments[_segments.Count - 1].position;
@@ -66,7 +93,7 @@ public class Snake : MonoBehaviour
         _segments.Add(segment);
     }
 
-    void ResetState()
+    private void ResetState()
     {
         _direction = Vector2.right;
         _nextDirection = Vector2.right;
@@ -87,22 +114,23 @@ public class Snake : MonoBehaviour
         this.transform.position = Vector3.zero;
     }
 
-    void Shrink()
+    private void Shrink()
     {
         Transform segment = _segments[_segments.Count - 1].transform;
         _segments.Remove(segment);
         Destroy(segment.gameObject);
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Food"))
         {
             Grow();
+            _gameManager.AddScore(1);
         }
         else if (other.CompareTag("Obstacle"))
         {
-            Debug.Log("Game Over");
+            _gameManager.EndGame();
         }
     }
 }
